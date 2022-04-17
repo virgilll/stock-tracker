cfbe83a95edacab7675d0c7fe25df55668208d88
Lucas Virgil <virgill@email.unc.edu>
2022-04-17 17:11:23 -0400

    create user-stocks backend

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -8,9 +12,11 @@ class Stock < ApplicationRecord
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
-      return nil
+      nil
     end
     # this is so that if anyone enters invalid ticker we make it equal to nil and then the controller sees that
     # and displays a message saying to enter valid ticker
   end
 end
+# user stocks is the database of users/stocks they track, so a stock can be on there
+# many times, therefore it has many instances of user_stocks, has many users through this
