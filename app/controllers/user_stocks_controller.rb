3be0163c761bf5701f45a891febb2bf708d9c1aa
Lucas Virgil <virgill@email.unc.edu>
2022-04-17 19:07:28 -0400

    track stock functionality

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,13 @@
 class UserStocksController < ApplicationController
+
+  def create
+    stock = Stock.findin_db(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      stock.save
+    end
+    @user_stock = UserStock.create( user: current_user, stock: stock)
+    flash[:notice] = "#{stock.name} added to portfolio!"
+    redirect_to my_portfolio_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def already_tracked?(ticker_symbol)
+    stock = Stock.findin_db(ticker_symbol)
+    # if stock already in database that means its being tracked so return true
+    return false unless stock
+    Stock.where(id: stock.id).exists?
+    # check if stock is already in db, if not check if it is already in portfolio
+  end
+  # these methods are used to check if a stock can be added to portfolio
+
+  def under_limit?
+    stocks.count < 10
+  end
+
+  def can_track?(ticker_symbol)
+    under_limit? && !already_tracked?(ticker_symbol)
+    # method used to check the two conditions in which stock cannot be tracked
+  end
 end
